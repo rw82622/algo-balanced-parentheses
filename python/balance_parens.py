900e16a3d221b67d3d5e1642ac3d06f3cab8e24c
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-29 17:31:14 -0500

    solve balance_parenthesis in python and create test cases in spec file

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,22 @@
-def balance_parens(str):
-    pass
+def balance_parens(arg):
+    #initialize your variables(PARENTHESIS, RESULT)
+    paranthesis = []
+    result = ''
+    # iterate through every character in ARG
+    for c in range(len(arg)):
+        # alpha-numeric characters get added to RESULT immediately
+        if arg[c].isalnum():
+            result += arg[c]
+        # add '(' to RESULT and PARENTHESIS only if there's a closing parenthesis in the remainder of ARG
+        elif arg[c] == '(' and ')' in arg[c+1:]:
+            paranthesis.append([len(result), arg[c]])
+            result += arg[c]
+        # add ')' to RESULT and pop PARENTHESIS only if PARENTHESIS isn't empty
+        elif arg[c] == ')' and len(paranthesis) > 0:
+            result += arg[c]
+            paranthesis.pop()
+    # After the for loop, remove extra '(' if there's one
+    if len(paranthesis) == 1:
+        index_to_pop = paranthesis[0][0]
+        result = result[0:index_to_pop] + result[index_to_pop+1:]
+    return result
--- python/balance_parens_spec.py
+++ python/balance_parens_spec.py
@@ -3,6 +3,16 @@ from balance_parens import balance_parens
 # Add more test cases!...
 print(balance_parens("abc(d)e(fgh))(i)j)k") == "abc(d)e(fgh)(i)jk")
 print(balance_parens("abc((d)e(fgh)(i)j(k") == "abc(d)e(fgh)(i)jk")
+print(balance_parens("()") == "()")
+print(balance_parens("a(b)c)") == "a(b)c")
+print(balance_parens("(a)(bdd)c)") == "(a)(bdd)c")
+print(balance_parens("a(dbvb)c)") == "a(dbvb)c")
+print(balance_parens("a(b)(c)())") == "a(b)(c)()")
+print(balance_parens(")(") == "")
+print(balance_parens("(((((") == "")
+print(balance_parens(")(())(") == "(())")
+print(balance_parens("(()()(") == "()()")
+print(balance_parens(")())(()()(") == "()()()")
 
 # Challenge: nested parentheses...
 print(balance_parens("abc(d)(ef(g(h))ij)k)lm()o)p") == "abc(d)(ef(g(h))ij)klm()op")
