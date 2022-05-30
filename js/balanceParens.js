b72ef107ddce00a6dbdcfccec4fb8eb7a7269105
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-29 19:13:14 -0500

    solve balanceParenthesis challenge in js and add more test cases to the spec file

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,25 @@
-balanceParens = (str) =>
-{
-
+balanceParens = (arg) => {
+// initialize your variables(PARENTHESIS, RESULT)
+let parentheses = []
+let result = ''
+// iterate through every character in ARG
+for (let c = 0; c < arg.length; c++){
+    // alpha-numeric characters get added to RESULT immediately
+    if (arg[c].match(/\w/)){
+        result += arg[c] }
+    // add '(' to RESULT and PARENTHESIS only if there's a closing parenthesis in the remainder of ARG
+    else if (arg[c] === '(' && arg.slice(c+1).includes(')')){
+        parentheses.push([result.length, arg[c]])
+        result += arg[c] }
+    // add ')' to RESULT and pop PARENTHESIS only if PARENTHESIS isn't empty
+    else if (arg[c] === ')' && parentheses.length > 0){
+        result += arg[c]
+        parentheses.pop() } }
+// After the for loop, remove extra '(' if there's one
+if (parentheses.length === 1){
+    let indexToPop = parentheses[0][0]
+    result = result.slice(0,indexToPop) + result.slice(indexToPop+1) }
+return result
 }
 
 module.exports = { balanceParens }
--- js/balanceParensSpec.js
+++ js/balanceParensSpec.js
@@ -1,8 +1,16 @@
 let bp = require("./balanceParens");
 
-// Add more test cases!...
 console.log(balanceParens("abc(d)e(fgh))(i)j)k") === "abc(d)e(fgh)(i)jk")
 console.log(balanceParens("abc((d)e(fgh)(i)j(k") === "abc(d)e(fgh)(i)jk")
+console.log(balanceParens("()") === "()")
+console.log(balanceParens("a(b)c)") === "a(b)c")
+console.log(balanceParens("(a)(bdd)c)") === "(a)(bdd)c")
+console.log(balanceParens("a(dbvb)c)") === "a(dbvb)c")
+console.log(balanceParens("a(b)(c)())") === "a(b)(c)()")
+console.log(balanceParens(")(") === "")
+console.log(balanceParens("(((((") === "")
+console.log(balanceParens(")(())(") === "(())")
+console.log(balanceParens("(()()(") === "()()")
+console.log(balanceParens(")())(()()(") === "()()()")
 
-// Challenge: nested parentheses...
 console.log(balanceParens("abc(d)(ef(g(h))ij)k)lm()o)p") === "abc(d)(ef(g(h))ij)klm()op")
